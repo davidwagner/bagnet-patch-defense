66bb4d300072eef77536a6ec5a99ec564f048979
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2019-05-27 22:33:46 -0700

    finding patches

--- code/bagnets/utils.py
+++ code/bagnets/utils.py
@@ -1,6 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import feature, transform
+from bagnets.pytorch import Bottleneck
+import torch.nn as nn
+import math
+from torch.utils import model_zoo
+
+model_urls = {
+            'bagnet9': 'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet8-34f4ccd2.pth.tar',
+            'bagnet17': 'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet16-105524de.pth.tar',
+            'bagnet33': 'https://bitbucket.org/wielandbrendel/bag-of-feature-pretrained-models/raw/249e8fa82c0913623a807d9d35eeab9da7dcc2a8/bagnet32-2ddd53ed.pth.tar',
+                            }
 
 def plot_heatmap(heatmap, original, ax, cmap='RdBu_r', 
                  percentile=99, dilation=0.5, alpha=0.25):
@@ -137,19 +147,16 @@ def extract_patches(image, patchsize):
     patches = patches.contiguous().view((-1, 3, patchsize, patchsize))
     return patches
 
-def bagnet_predict(bagnet, patches, device, batch_size=1000, return_class=True):
+def bagnet_predict(model, images, device, return_class=True):
     with torch.no_grad():
-        cum_logits = torch.zeros(1000).to(device) # ImageNet has 1000 classes
-        for batch_patches in torch.split(patches, batch_size):
-            logits = bagnet(batch_patches)
-            
-            sum_logits = torch.sum(logits, dim=0)
-            cum_logits += sum_logits
-        p = F.softmax(cum_logits/(244*244), dim=0)
+        images = torch.from_numpy(images).to(device)
+        logits = model(images)
+        p = torch.nn.Softmax(dim=1)(logits)
+        p = p.cpu().numpy()
         if return_class:
-            return torch.argmax(p).item()
+            return np.argmax(p, axis=1)
         else:
-            return p.cpu().numpy()
+            return p
 
 def compare_heatmap(bagnet, patches, gt, target, original, batch_size=1000):
     with torch.no_grad():
@@ -212,3 +219,321 @@ def attack_patch(image, patchsize, num_patches, seed=None):
         c, h, w = image[:, (xi - (patchsize-1)//2): (xi + (patchsize-1)//2), (yi - (patchsize-1) // 2): (yi + (patchsize-1) // 2)].shape
         image[:, (xi - (patchsize-1)//2): (xi + (patchsize-1)//2), (yi - (patchsize-1) // 2): (yi + (patchsize-1) // 2)] = np.random.rand(c, h, w)
     return image
+###################################################
+
+###################################################
+# Helper function from 5-26 notebook
+###################################################
+def compute_saliency_map(images, labels, model, criterion, device):
+    images, labels = torch.from_numpy(images).to(device), torch.from_numpy(labels).to(device)
+    images.requires_grad_(True)
+    logits = model(images)
+    loss = criterion(logits, labels)
+    loss.backward()
+    saliency = np.amax(np.absolute(images.grad.cpu().numpy()), axis=1)
+    return saliency
+
+def plot_saliency(images, saliency):
+    for i in range(len(saliency)):
+        fig = plt.figure(figsize=(8, 4))
+        ax = plt.subplot(121)
+        ax.set_title('original')
+        plt.imshow(convert2channel_last(images[i]))
+        plt.axis('off')
+
+        ax = plt.subplot(122)
+        ax.set_title('saliency map')
+        plt.imshow((saliency[i]), cmap=plt.cm.hot)
+        plt.axis('off')
+        plt.show()
+        
+class BottleneckDebug(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, kernel_size=1):
+        super(BottleneckDebug, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=kernel_size, stride=stride,
+                               padding=0, bias=False) # changed padding from (kernel_size - 1) // 2
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x, **kwargs):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            print('Bottleneck: shape before downsampling {}'.format(x.shape))
+            residual = self.downsample(x)
+        
+        if residual.size(-1) != out.size(-1):
+            print('Bottleneck: shape after downsampling {}'.format(residual.shape))
+            print('Bottleneck: shape of out {}'.format(out.shape))
+            diff = residual.size(-1) - out.size(-1)
+            residual = residual[:,:,:-diff,:-diff]
+        
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class BagNetDebug(nn.Module):
+
+    def __init__(self, block, layers, strides=[1, 2, 2, 2], kernel3=[0, 0, 0, 0], num_classes=1000, avg_pool=True):
+        self.inplanes = 64
+        super(BagNetDebug, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=1, stride=1, padding=0,
+                               bias=False)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64, momentum=0.001)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=strides[0], kernel3=kernel3[0], prefix='layer1')
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=strides[1], kernel3=kernel3[1], prefix='layer2')
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=strides[2], kernel3=kernel3[2], prefix='layer3')
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=strides[3], kernel3=kernel3[3], prefix='layer4')
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.avg_pool = avg_pool
+        self.block = block
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1, kernel3=0, prefix=''):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        kernel = 1 if kernel3 == 0 else 3
+        layers.append(block(self.inplanes, planes, stride, downsample, kernel_size=kernel))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            kernel = 1 if kernel3 <= i else 3
+            layers.append(block(self.inplanes, planes, kernel_size=kernel))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        print('BagNet: shape after conv1 {}\n'.format(x.shape))
+        x = self.conv2(x)
+        print('BagNet: shape after conv2 {}\n'.format(x.shape))
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        print('BagNet: shape after layer1 {}\n'.format(x.shape))
+        x = self.layer2(x)
+        print('BagNet: shape after layer2 {}\n'.format(x.shape))
+        x = self.layer3(x)
+        print('BagNet: shape after layer3 {}\n'.format(x.shape))
+        x = self.layer4(x)
+        print('BagNet: shape after layer4 {}\n'.format(x.shape))
+
+        if self.avg_pool:
+            print('BagNet: kernel size of AvgPool2d: {}'.format(x.size()[2]))
+            x = nn.AvgPool2d(x.size()[2], stride=1)(x)
+            x = x.view(x.size(0), -1)
+            print('BagNet: shape after flattening: {}'.format(x.shape))
+            x = self.fc(x)
+            print('BagNet: shape of final output: {}'.format(x.shape))
+        else:
+            x = x.permute(0,2,3,1)
+            x = self.fc(x)
+
+        return x
+
+def bagnet33_debug(pretrained=False, strides=[2, 2, 2, 1], **kwargs):
+    """Constructs a Bagnet-33 model (Debugging mode).
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = BagNetDebug(BottleneckDebug, [3, 4, 6, 3], strides=strides, kernel3=[1,1,1,1], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['bagnet33']))
+    return model
+###################################################
+
+###################################################
+# Helper function from 5-27 notebook
+###################################################
+def bagnet_patch_predict(bagnet, patches, device, batch_size=256, return_class=True):
+    """ Take a batch of patches as input, and make prediction using bagnet.
+    """
+    N, M, C, P, P = patches.shape
+    with torch.no_grad():
+        cum_logits = torch.zeros(N, 1000).to(device) # ImageNet has 1000 classes
+        for n in range(N):
+            for batch_patches in torch.split(patches[n], batch_size):
+                logits = bagnet(batch_patches)
+                sum_logits = torch.sum(logits, dim=0)
+                cum_logits[n, :] += sum_logits
+        p = F.softmax(cum_logits/M, dim=1)
+        if return_class:
+            return torch.argmax(p, dim=1)
+        else:
+            return p.cpu().numpy()
+        
+class BottleneckRF(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, kernel_size=1):
+        super(BottleneckRF, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=kernel_size, stride=stride,
+                               padding=0, bias=False) # changed padding from (kernel_size - 1) // 2
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x, **kwargs):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        
+        if residual.size(-1) != out.size(-1):
+            diff = residual.size(-1) - out.size(-1)
+            residual = residual[:,:,:-diff,:-diff]
+        
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class MaskLayer(nn.Module):
+    
+    def __init__(self, shape, coordinate, device):
+        super(MaskLayer, self).__init__()
+        X, Y = coordinate
+        self.mask = torch.zeros(shape).to(device)
+        for x in X:
+            for y in Y:
+                self.mask[:, :, x, y] = 1
+        
+    def forward(self, x):
+        return x*self.mask
+
+
+class BagNetRF(nn.Module):
+
+    def __init__(self, block, mask, layers, strides=[1, 2, 2, 2], kernel3=[0, 0, 0, 0], num_classes=1000, avg_pool=True):
+        self.inplanes = 64
+        super(BagNetRF, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=1, stride=1, padding=0,
+                               bias=False)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64, momentum=0.001)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=strides[0], kernel3=kernel3[0], prefix='layer1')
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=strides[1], kernel3=kernel3[1], prefix='layer2')
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=strides[2], kernel3=kernel3[2], prefix='layer3')
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=strides[3], kernel3=kernel3[3], prefix='layer4')
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.avg_pool = avg_pool
+        self.block = block
+        self.mask = mask
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1, kernel3=0, prefix=''):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        kernel = 1 if kernel3 == 0 else 3
+        layers.append(block(self.inplanes, planes, stride, downsample, kernel_size=kernel))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            kernel = 1 if kernel3 <= i else 3
+            layers.append(block(self.inplanes, planes, kernel_size=kernel))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        print('BagNet: shape after layer4 {}\n'.format(x.shape))
+        x = self.mask(x)
+
+        if self.avg_pool:
+            x = nn.AvgPool2d(x.size()[2], stride=1)(x)
+            x = x.view(x.size(0), -1)
+            x = self.fc(x)
+        else:
+            x = x.permute(0,2,3,1)
+            x = self.fc(x)
+
+        return x
+
+def bagnet33_RF(batch_size, coordinate, device, pretrained=False, strides=[2, 2, 2, 1], **kwargs):
+    """Constructs a Bagnet-33 model (Receptive field mode).
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    mask = MaskLayer((batch_size, 2048, 24, 24), coordinate, device)
+    
+    model = BagNetRF(BottleneckRF, mask, [3, 4, 6, 3], strides=strides, kernel3=[1,1,1,1], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['bagnet33']))
+    return model
+###################################################
