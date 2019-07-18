b29f959b4edf5f824c8afc0dd86197334af78552
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2019-07-18 22:40:11 +0000

    binarization simulation

--- code/run_exp.sh
+++ code/run_exp.sh
@@ -1,4 +1,6 @@
-# 300 images 50-20x20-20-tanh_linear-0.05--1.0-5.0-40-0.5 (Finished)
+####################################################################
+# 50x6 images 20x20-20-tanh_linear-0.05--1.0-5.0-40-0.5 (Finished)
+####################################################################
 #sudo python3 generate_new_metabatch.py --seed=42 --metabatch_size=10
 #sudo python3 generate_new_metabatch.py --seed=88 --metabatch_size=10
 #sudo python3 generate_new_metabatch.py --seed=1234 --metabatch_size=10
@@ -6,18 +8,22 @@
 #sudo python3 generate_new_metabatch.py --seed=777 --metabatch_size=10
 #sudo python3 generate_new_metabatch.py --seed=999 --metabatch_size=10
 
+####################################################################
+# 500 images 20x20-20-tanh_linear-0.05--1.0-5.0-40-0.5 (Finished)
+####################################################################
+#sudo python3 generate_new_metabatch.py --N=500 --seed=42 --metabatch_size=10
 
-# 500 images 50-20x20-20-tanh_linear-0.05--1.0-5.0-40-0.5 (Plan)
-sudo python3 generate_new_metabatch.py --N=500 --seed=42 --metabatch_size=10
+####################################################################
+# 500 images 20x20-20-sigmoid_linear-1000.0--25000-5.0-40-0.5 (Planed)
+####################################################################
+sudo python3 generate_new_metabatch.py --N=500 --seed=42 --clip_fn=sigmoid_linear --a=100000.0 --b=-2500000.0 --metabatch_size=10
 
-
-# 500 images 50-20x20-20-sigmoid_linear-1000.0--0.025-5.0-40-0.5 (Plan)
-sudo python3 generate_new_metabatch.py --N=500 --seed=42 --clip_fn=sigmoid_linear --a=1000.0 --b=0.025 --metabatch_size=10
-
-# 300 images 50-20x20-20-tanh_linear-0.05--1.0-5.0-120-0.5
+#####################################################################
+# 50x6 images 20x20-20-tanh_linear-0.05--1.0-5.0-120-0.5 (In progress)
+#####################################################################
 #sudo python3 generate_new_metabatch.py --seed=666 --nb_iter=120 --metabatch_size=10
-sudo python3 generate_new_metabatch.py --seed=1234 --nb_iter=120 --metabatch_size=10
-sudo python3 generate_new_metabatch.py --seed=42 --nb_iter=120 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=1234 --nb_iter=120 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=42 --nb_iter=120 --metabatch_size=10
 sudo python3 generate_new_metabatch.py --seed=88 --nb_iter=120 --metabatch_size=10
 sudo python3 generate_new_metabatch.py --seed=777 --nb_iter=120 --metabatch_size=10
 sudo python3 generate_new_metabatch.py --seed=999 --nb_iter=120 --metabatch_size=10
