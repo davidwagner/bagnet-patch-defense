d35c6d02a7cae9cb5628e1d4bd20959db037ded8
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2019-07-18 07:27:05 +0000

    more exp

---
+++ code/check_repetition.py
@@ -0,0 +1,30 @@
+import numpy as np
+from absl import app, flags
+import os
+import logging
+
+FLAGS = flags.FLAGS
+flags.DEFINE_multi_integer("seed_list", [42, 88, 1234, 666, 777, 999], 'list of random seeds')
+flags.DEFINE_string('log_root', '/mnt/data/results/', 'directory for storing results')
+def main(argv):
+    NAME = "{}".format(FLAGS.seed_list)
+    LOG_PATH = os.path.join(FLAGS.log_root, NAME+'.log')
+    print("log to {}".format(LOG_PATH))
+
+    logger = logging.basicConfig(filename=LOG_PATH, level=logging.INFO)
+    total = []
+    for seed in FLAGS.seed_list:
+        np.random.seed(seed)
+        sub = np.random.choice(np.arange(50000), size=FLAGS.N, replace=False)
+        logging.info("seed: {} \n indices: {}".format(seed, sub))
+        total.append(sub[:])
+    total = np.array(total).flatten()
+    unique, count = np.unique(total, return_counts=True)
+    has_rep = np.any([i != 1 for i in count])
+    logging.info("has repetition: {}".format(has_rep))
+    print(has_rep)
+    return has_rep
+
+
+if __name__ == "__name__":
+    app.run(main)
--- code/run_exp.sh
+++ code/run_exp.sh
@@ -1,3 +1,21 @@
-sudo python3 generate_new_metabatch.py --seed=42 --metabatch_size=10
-sudo python3 generate_new_metabatch.py --seed=88 --metabatch_size=10
-sudo python3 generate_new_metabatch.py --seed=1234 --metabatch_size=10
+# 300 images 50-20x20-20-tanh_linear-0.05--1.0-5.0-40-0.5 (Finished)
+#sudo python3 generate_new_metabatch.py --seed=42 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=88 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=1234 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=666 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=777 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=999 --metabatch_size=10
+
+
+# 500 images 50-20x20-20-tanh_linear-0.05--1.0-5.0-40-0.5 (Plan)
+#sudo python3 generate_new_metabatch.py --N=500 --seed=42 --metabatch_size=10
+
+# 50 images 50-20x20-20-tanh_linear-0.05--1.0-5.0-120-0.5
+#sudo python3 generate_new_metabatch.py --seed=666 --nb_iter=120 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=1234 --nb_iter=120 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=42 --nb_iter=120 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=88 --nb_iter=120 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=777 --nb_iter=120 --metabatch_size=10
+#sudo python3 generate_new_metabatch.py --seed=999 --nb_iter=120 --metabatch_size=10
+
+
