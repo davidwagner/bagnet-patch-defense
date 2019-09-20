e4400ce753d11cce83f805252884a7c4105cf27c
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2019-09-20 03:51:41 +0000

    branch for spsa model

--- code/attacks/spsa/utils.py
+++ code/attacks/spsa/utils.py
@@ -195,6 +195,66 @@ class StickerSPSA:
         # Step 3: Preprocess the adversarial subimage
         self.adv_subimg = (adv_undo_subimg - self.mean) / self.std
 
+class StickerSPSAModel(nn.Module):
+    def __init__(self, model, subimg, label, sticker_size=(20, 20), 
+                 delta = 0.01, num_samples=128, step_size=0.01):
+        super(StickerSPSAModel, self).__init__()
+        self.model = model
+        self.clean_subimg = subimg.clone()
+        self.clean_undo_subimg = undo_imagenet_preprocess_pytorch(subimg)
+        self.mean = torch.tensor([0.485, 0.456, 0.406]).reshape((1, 3, 1, 1)).cuda()
+        self.std = torch.tensor([[0.229, 0.224, 0.225]]).reshape((1, 3, 1, 1)).cuda()
+
+        self.adv_subimg = subimg.clone()
+        self.label = label
+        self.sticker_size = sticker_size
+        self.adv_pertub = None
+        self.num_samples = num_samples
+        self.delta = delta
+        self.adam_optimizer = AdamOptimizer(shape=(1, 3)+sticker_size, learning_rate=step_size)
+    
+    def forward(self, adv_subimg):
+        # Sample perturbation from Bernoulli +/- 1 distribution
+        _samples = torch.sign(torch.empty((self.num_samples//2, 3) + self.sticker_size, dtype=self.adv_subimg.dtype).uniform_(-1, 1))
+        _samples = _samples.cuda()
+        delta_x = self.delta * _samples
+        delta_x = torch.cat([delta_x, -delta_x], dim=0) # so there are 2*num_samples
+        _sampled_perturb = self.adv_subimg + delta_x
+
+        with torch.no_grad():
+            logits = self.model(_sampled_perturb)
+
+        # calculate the margin logit loss
+        label_logit = logits[:, self.label].reshape((-1, ))
+        value, indices = torch.topk(logits, k=5, dim=1)
+        logits[:, self.label] = float('-inf')
+        best_other_logit, _ = torch.max(logits, dim=1)
+        ml_loss = label_logit - best_other_logit
+
+        # estimate the gradient
+        all_grad = ml_loss.reshape((-1, 1, 1, 1)) / delta_x
+        est_grad = torch.mean(all_grad, dim=0)
+
+        # update the sticker
+        self.adv_subimg += self.adam_optimizer(est_grad[None])
+
+        # Clip the perturbation so that it is in a valid range
+        # There several steps:
+
+        # Step 1: Make sure the pertubation by itself is valid.
+        adv_undo_subimg = undo_imagenet_preprocess_pytorch(self.adv_subimg)
+        adv_undo_pertub = adv_undo_subimg - self.clean_undo_subimg 
+        adv_undo_pertub = torch.clamp(adv_undo_pertub, 0, 1)
+
+        # Step 2: Make sure the image with sticker is valid.
+        adv_undo_subimg = adv_undo_pertub + self.clean_undo_subimg
+        adv_undo_subimg = torch.clamp(adv_undo_subimg, 0, 1)
+
+        # Step 3: Preprocess the adversarial subimage
+        self.adv_subimg = (adv_undo_subimg - self.mean) / self.std
+        return self.adv_subimg
+
+
 def apply_sticker(adv, img, loc, size):
     """
     Input:
